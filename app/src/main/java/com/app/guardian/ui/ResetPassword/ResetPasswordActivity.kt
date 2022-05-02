b36090c03923adc568a39fef132ed936640b6f7d
cnsarita <sarita@capitalnumbers.com>
2022-05-02 17:51:05 +0530

    Fix - Reset password screen pending - Login screen design - Forgot password screen create - All screen with email field pending

---
+++ app/src/main/java/com/app/guardian/ui/ResetPassword/ResetPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.app.guardian.ui.ResetPassword
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.guardian.R
+
+class ResetPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reset_password)
+    }
+}
