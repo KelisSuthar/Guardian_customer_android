93f29acf0b9eb2ed202cc57e91ea44c6961ea6b7
cnsarita <sarita@capitalnumbers.com>
2022-05-02 16:26:41 +0530

    Fix - Login screen design

--- app/src/main/java/com/app/guardian/ui/Login/LoginActivity.kt
+++ app/src/main/java/com/app/guardian/ui/Login/LoginActivity.kt
@@ -3,10 +3,12 @@ package com.app.guardian.ui.Login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.app.guardian.R
+import com.app.guardian.common.ReusedMethod
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        ReusedMethod.updateStatusBarColor(this,R.color.colorPrimaryDark,4)
     }
 }
