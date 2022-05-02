206f3977fd3fc0d2b0420592de647155a1154616
cnsarita <sarita@capitalnumbers.com>
2022-05-02 14:43:55 +0530

    Fix -Login activity create -Manifest file lunch tag replace with login activity.

---
+++ app/src/main/java/com/app/guardian/ui/Login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.app.guardian.ui.Login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.app.guardian.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
