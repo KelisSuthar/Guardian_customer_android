f84fbdfaa1a14dc7a7a30d082c5e44d0bc4d6804
SaritaIQL <sarita.iqlance@gmail.com>
2022-05-31 14:31:07 +0530

    Fix - commit for push

--- app/src/main/java/com/app/guardian/ui/chatting/ChattingFragment.kt
+++ app/src/main/java/com/app/guardian/ui/chatting/ChattingFragment.kt
@@ -24,16 +24,6 @@ import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ChattingFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 
 class ChattingFragment(selectUserId: Int,var selectUserFullName: String, profilePicUrl: String) : BaseFragment(), View.OnClickListener {
     lateinit var mBinding: FragmentChattingBinding
